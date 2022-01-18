cb31e7ea57ecb66fd0108a03a9369275af221472
fuck <fuckyou@now.com>
2022-01-18 17:50:54 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/flutter_poem/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_poem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/util/poemRouter.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_poem/page/homePage.dart';
+import 'package:flutter_poem/page/searchPage.dart';
+import 'package:flutter_poem/page/minePage.dart';
+
+class PoemRouter extends StatefulWidget {
+  const PoemRouter({Key? key}) : super(key: key);
+
+  @override
+  State<PoemRouter> createState() => _PoemRouterState();
+}
+
+class _PoemRouterState extends State<PoemRouter> {
+  
+  final List<BottomNavigationBarItem> bottomNavBarItems = [
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: 'Home')
+    , BottomNavigationBarItem(icon: Icon(CupertinoIcons.search), label: 'Search')
+    , BottomNavigationBarItem(icon: Icon(CupertinoIcons.profile_circled), label: 'Me')
+  ];
+  
+  final bottomNavBarItemBodies = [
+    const MyHomePage(title: 'SilentChan')
+    ,SearchPage()
+    ,MinePage()
+  ];
+  
+  int currentPageIndex = 0;
+  var currentPageObj;
+
+  @override
+  void initState() {
+    currentPageObj = bottomNavBarItemBodies[currentPageIndex];
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(244, 245, 245, 1.0)
+      , bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed
+        , currentIndex: currentPageIndex
+        , items: bottomNavBarItems
+        , onTap: (index) {
+          setState(() {
+            currentPageIndex = index;
+            currentPageObj   = bottomNavBarItemBodies[currentPageIndex];
+          });
+        }
+      )
+      , body: currentPageObj
+    );
+  }
+}
